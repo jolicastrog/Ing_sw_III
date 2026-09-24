ebc1bd68935e93c7931fdabf266a97c1ed4c9f09
jlcastrog <jlizardocastro@gmail.com>
2026-09-23 19:21:03 -0500

    desa-jlcastrog-Implementacion procedimiento de iniciar session

---
+++ baseDatos/Procedimientos/proc_iniciar_sesion.sql
@@ -0,0 +1,13 @@
+create or replace procedure proc_iniciar_sesion(
+   in p_sesion varchar)
+language plpgsql
+as $body$
+DECLARE
+ p_sesion := '';
+BEGIN
+   p_sesion := 'sdferwvsgf' ;
+end;
+$body$   
+
+
+ 
